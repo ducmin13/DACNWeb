b4821b2e5fdcad25e0c845915663464e4a85a03a
Min <ngoducminh1305@gmail.com>
2022-10-25 19:35:31 +0700

    Them admin

---
+++ Controllers/DatXeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnChuyenNganh.Controllers
+{
+    public class DatXeController : Controller
+    {
+        // GET: DatXe
+        public ActionResult DatXe()
+        {
+            return View();
+        }
+    }
+}
