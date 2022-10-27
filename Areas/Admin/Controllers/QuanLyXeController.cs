b3f17513cbef81c5ccd44a07a7d6ddfb2e37ed83
ThanhPhamCoder <99668673+ThanhPhamCoder@users.noreply.github.com>
2022-10-27 22:56:51 +0700

    Thanh commit quan ly xe

---
+++ Areas/Admin/Controllers/QuanLyXeController.cs
@@ -0,0 +1,20 @@
+﻿using DoAnChuyenNganh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnChuyenNganh.Areas.Admin.Controllers
+{
+    public class QuanLyXeController : Controller
+    {
+        DoAnChuyenNganhContext db = new DoAnChuyenNganhContext();
+        // GET: Admin/Xe
+        public ActionResult Index()
+        {        
+            return View(db.Xes.ToList());
+        }
+
+    }
+}
