9bb107340d81bee9740dcaa25b25a414dfcdfac3
Thanh <99668673+ThanhPhamCoder@users.noreply.github.com>
2022-11-12 15:21:52 +0700

    moi

---
+++ Controllers/TimKiemController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAnChuyenNganh.Models;
+namespace DoAnChuyenNganh.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        DoAnChuyenNganhContext db = new DoAnChuyenNganhContext();
+        // GET: TimKirm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CoTaiXe()
+        {
+            return  View();
+        }
+        public ActionResult XacNhanXe(int id)
+        {
+            var xecotaixe = db.LoaiXes.Where(n => n.TenLoaiXe != null).OrderBy(n => n.MaLoaiXe);
+            if (!string.IsNullOrEmpty(searchString)) all_loaixe =
+            (IOrderedQueryable<LoaiXe>)all_loaixe.Where(a => a.TenLoaiXe.Contains(searchString));
+
+            return View(all_loaixe);
+        }
+    }
+}
